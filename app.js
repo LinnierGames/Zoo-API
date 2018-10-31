58e2ac217880e7d18c98310117853b4cee265fa6
LinnierGames <esericksanc@gmail.com>
2018-10-31 11:14:51 -0700

    generate project

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require( 'express-handlebars');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// view engine setup
+app.set('view engine', 'hbs');
+
+app.engine( 'hbs', hbs( {
+  extname: 'hbs',
+  defaultView: 'default',
+  layoutsDir: __dirname + '/views/pages/',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+module.exports = app;
